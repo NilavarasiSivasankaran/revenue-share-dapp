2f6c311f83b0936b24f625326ecc5a290d8da3d1
kavi-clustrex <kavi@clustrex.com>
2017-06-09 04:47:33 +0000

    added abi info dir

---
+++ abi/RevenueShareContract.js
@@ -0,0 +1,3 @@
+var abi = '[{"constant":false,"inputs":[],"name":"splitRevenue","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[],"name":"revToContract","outputs":[],"payable":true,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}]';
+var address = '0x7c37727da53968955f8cb0add5694aa996dee002';
+module.exports = {abi: abi, address: address};
